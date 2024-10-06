a0bd75d346a8267e45831e2203e99f6f0db63957
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-10-06 18:22:30 +0400

    Declare token store

--- Persistense/Stores/TokenStore.cs
+++ Persistense/Stores/TokenStore.cs
@@ -2,6 +2,7 @@
 using CSharpFunctionalExtensions;
 using Domain.Entitys;
 using Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +13,150 @@ namespace Persistense.Stores
 {
     public class TokenStore : ITokenStore
     {
+        private readonly ApplicationDBContext _dbcontext;
+
+        public TokenStore(ApplicationDBContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
         public async Task<Result> DeactivateTokens(Tokens tokens)
         {
-            return Result.Failure("Not realised");
-            throw new NotImplementedException();
+            if (tokens == null)
+            {
+                return Result.Failure("null tokens");
+            }
+
+            if(await _dbcontext.Database.CanConnectAsync() == false)
+            {
+                return Result.Failure("database unawaliable");
+            }
+
+            try
+            {
+                var getTokenFromDBResult = await this.GetById(tokens.Id);
+
+                if (getTokenFromDBResult.IsFailure)
+                {
+                    return getTokenFromDBResult.ConvertFailure();
+                }
+
+                _dbcontext.Remove(getTokenFromDBResult.Value);
+
+                await _dbcontext.SaveChangesAsync();
+
+                return Result.Success();
+            }
+            catch(Exception ex)
+            {
+                return Result.Failure(ex.Message);
+            }
+        }
+        public async Task<Result> DeactivateTokens(Guid tokensId)
+        {
+
+            if (await _dbcontext.Database.CanConnectAsync() == false)
+            {
+                return Result.Failure("database unawaliable");
+            }
+
+            try
+            {
+                var getTokenFromDBResult = await this.GetById(tokensId);
+
+                if (getTokenFromDBResult.IsFailure)
+                {
+                    return getTokenFromDBResult.ConvertFailure();
+                }
+
+                _dbcontext.Remove(getTokenFromDBResult.Value);
+
+                await _dbcontext.SaveChangesAsync();
+
+                return Result.Success();
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure(ex.Message);
+            }
         }
 
         public async Task<Result<bool>> IsActiveToken(Token token)
         {
-            return Result.Failure<bool>("Not realised");
-            throw new NotImplementedException();
+            if(await _dbcontext.Database.CanConnectAsync() == false)
+            {
+                return Result.Failure<bool>("database unawaliable");
+            }
+
+            try
+            {
+                var recordByToken = await _dbcontext
+                    .Tokens
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(ex => ex.TokensPair.RefreshToken.Token == token || ex.TokensPair.Auth.Token == token);
+
+                if(recordByToken == default)
+                {
+                    return Result.Failure<bool>($"dont contain record with token {token.Value}");
+                }
+
+                return Result.Success(recordByToken.IsDisabled == false);
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure<bool>(ex.Message);
+            }
+
+        }
+
+        public async Task<Result<Tokens>> GetById(Guid id)
+        {
+            if (await _dbcontext.Database.CanConnectAsync() == false)
+            {
+                return Result.Failure<Tokens>("database unawaliable");
+            }
+
+            try
+            {
+                var token = await _dbcontext
+                    .Tokens
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(ex => ex.Id == id);
+
+                if(token == default)
+                {
+                    return Result.Failure<Tokens>($"database dont contain token with id {id}");
+                }
+
+
+                return Result.Success(token);
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure<Tokens>(ex.Message);
+            }
         }
 
         public async Task<Result> SaveTokens(Tokens tokens)
         {
-            return Result.Failure("Not realised");
-            throw new NotImplementedException();
+            if(await _dbcontext.Database.CanConnectAsync() == false)
+            {
+                return Result.Failure("database unawaliable");
+            }
+
+            try
+            {
+                await _dbcontext.AddAsync(tokens);
+
+                await _dbcontext.SaveChangesAsync();
+
+                return Result.Success();
+
+            }
+            catch (Exception ex)
+            {
+                return Result.Failure(ex.Message);
+            }
         }
     }
 }
