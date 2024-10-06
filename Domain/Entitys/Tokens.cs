b2bb1c41c6d957c205b86081f5f63cad9e6ce159
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-10-06 18:23:46 +0400

    Make domein entitys reaches

--- Domain/Entitys/Tokens.cs
+++ Domain/Entitys/Tokens.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AuntificationService.Domain.Entitys;
 using AuntificationService.Domain.ValueObjects;
 using CSharpFunctionalExtensions;
 using Domain.ValueObjects;
@@ -11,7 +12,62 @@ namespace Domain.Entitys
 {
     public class Tokens : Entity<Guid>
     {
-        public DateTime CreateDate { get; set; }
-        public TokensPair TokensPair { get; set; }
+        public bool IsDisabled { get; private set; }
+        public DateTime CreateDate { get; init; }
+        public TokensPair TokensPair { get; init; }
+
+        public User Owner { get; private set; }
+
+        private Tokens(Guid id, bool isDisabled, DateTime createDate, TokensPair tokens, User owner)
+        {
+            Id = id;
+            IsDisabled = isDisabled;
+            CreateDate = createDate;
+            TokensPair = tokens;
+            Owner = owner;
+        }
+
+        public static Result<Tokens> Create(Guid id, bool isDisabled, DateTime createDate, TokensPair tokens, User owner)
+        {
+            if(tokens == null)
+            {
+                return Result.Failure<Tokens>("Empty tokens");
+            }
+
+            return Result.Success(new Tokens(id, isDisabled, createDate, tokens, owner));
+        }
+        public static Result<Tokens> Create(bool isDisabled, DateTime createDate, TokensPair tokens, User owner)
+        {
+            if (tokens == null)
+            {
+                return Result.Failure<Tokens>("Empty tokens");
+            }
+
+            var id = Guid.NewGuid();
+
+            return Result.Success(new Tokens(id, isDisabled, createDate, tokens, owner));
+        }
+        public static Result<Tokens> Create(DateTime createDate, TokensPair tokens, User owner)
+        {
+            if (tokens == null)
+            {
+                return Result.Failure<Tokens>("Empty tokens");
+            }
+
+            var id = Guid.NewGuid();
+
+            return Result.Success(new Tokens(id, false, createDate, tokens, owner));
+        }
+
+        public Result Deactivate()
+        {
+            if (IsDisabled)
+            {
+                return Result.Failure("Token already disabled");
+            }
+            IsDisabled = true;
+            return Result.Success();
+        }
+
     }
 }
--- Domain/Entitys/User.cs
+++ Domain/Entitys/User.cs
@@ -11,9 +11,42 @@ namespace AuntificationService.Domain.Entitys
 {
     public class User : Entity<Guid>
     {
+        private readonly List<Tokens> _tokens;
+
         public UserRole Role { get; private set; }
         public AuthCredentials Credentials { get; init; }
-        
-        
+
+        public IReadOnlyCollection<Tokens> OwnedTokens => _tokens;
+
+        private User(Guid id, UserRole role, AuthCredentials credentials, List<Tokens> tokens)
+        {
+            Id = id;
+            Role = role;
+            Credentials = credentials;
+            _tokens = tokens;
+        }
+
+        public static Result<User> Create(Guid id, UserRole role, AuthCredentials credentials, List<Tokens> tokens)
+        {
+            if (credentials == null)
+            {
+                return Result.Failure<User>("empty credentials");
+            }
+
+            return Result.Success(new User(id, role, credentials, tokens));
+        }
+
+        public static Result<User> Create(UserRole role, AuthCredentials credentials, List<Tokens> tokens)
+        {
+            if (credentials == null)
+            {
+                return Result.Failure<User>("empty credentials");
+            }
+
+            var id = Guid.NewGuid();
+
+            return Result.Success(new User(id, role, credentials, tokens));
+        }
+
     }
 }
