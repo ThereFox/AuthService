2eb90cfd3174965628714a25e1906537d622fc62
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-10-10 15:17:58 +0400

    Create token pair create method

--- Domain/ValueObjects/TokensPair.cs
+++ Domain/ValueObjects/TokensPair.cs
@@ -10,13 +10,24 @@ namespace Domain.ValueObjects
 {
     public class TokensPair : ValueObject
     {
-        public AuthorisationToken Auth { get; set; }
+        public AuthorisationToken AuthToken { get; set; }
         public RefreshToken RefreshToken { get; set; }
 
         protected override IEnumerable<object> GetEqualityComponents()
         {
-            yield return Auth.Token;
+            yield return AuthToken.Token;
             yield return RefreshToken.Token;
+        }
+
+        private TokensPair(AuthorisationToken authToken, RefreshToken refreshToken)
+        {
+            AuthToken = authToken;
+            RefreshToken = refreshToken;
+        }
+
+        public static Result<TokensPair> Create(AuthorisationToken auth, RefreshToken refreshToken)
+        {
+            return Result.Success(new TokensPair(auth, refreshToken));
         }
     }
 }
