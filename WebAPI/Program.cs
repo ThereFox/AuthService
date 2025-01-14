d08526aedcf7df3e0a6fbc801c5e30c90635a3c8
ThereFox <alexandrgrizunow@gmail.com>
2025-01-14 21:02:19 +0400

    segregete ports for grpc and common controllers

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using Application.DI;
 using Infrastructure.Tokens.Getter;
 using Infrastructure.Tokens.JWT;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistense.DI;
 using WebAPI.Configurations;
@@ -8,15 +9,28 @@ using WebAPI.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(
+    kestrel =>
+    {
+        kestrel.ListenAnyIP(5279, options =>
+        {
+            options.Protocols = HttpProtocols.Http1;
+        });
+        kestrel.ListenAnyIP(6000, options =>
+        {
+            options.Protocols = HttpProtocols.Http2;
+        });
+    });
+
 var configuration = builder.Configuration.GetSection("ServicesConfigs").Get<ServiceConfiguration>();
 
-//builder.Services.AddGrpc();
+builder.Services.AddGrpc();
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services
     .AddApplication()
-    .AddHttpTokenGetter("Test", "test")
+    .AddHttpTokenGetter("AuthToken", "RefreshToken")
     .AddPersistense(
     ex => ex.UseNpgsql(configuration.MainDatabase.ConnectionString).EnableDetailedErrors()
     )
@@ -25,7 +39,7 @@ builder.Services
 var app = builder.Build();
 
 app.UseRouting();
-//app.MapGrpcService<GetInfoController>().RequireHost("*:6000");
-app.MapControllers();
+app.MapGrpcService<GetInfoController>().RequireHost("*:6000");
+app.MapControllers().RequireHost("*:5279");
 
 app.Run();
